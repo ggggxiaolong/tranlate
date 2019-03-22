9636d2949a0d7c7cf9b5a9a2d0fbde308d5ba37e
mrtan <freemrtan@gmail.com>
2019-03-22 18:10:34 +0800

    Add mutation user

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -9,8 +9,39 @@ export const resoler = {
             return users;
         },
         user: async (_, {id}, __) => {
-            const userRepository = getRepository(User)
+            const userRepository = getRepository(User);
             return await userRepository.findOne({id: parseInt(id)});
-        }
-    }
+        },
+    },
+    Mutation: {
+        addUser: async (_, {firstName, lastName, age}, __) => {
+            const userRepository = getRepository(User);
+            const user = new User();
+            user.firstName = firstName;
+            user.lastName = lastName;
+            user.age = age;
+            await userRepository.insert(user);
+            return user;
+        },
+        updateUser: async (_, {id, firstName, lastName, age}, __) => {
+            const userRepository = getRepository(User);
+            const user = await userRepository.findOne({id:id});
+            if(firstName){
+                user.firstName = firstName;
+            }
+            if(lastName){
+                user.lastName = lastName;
+            }
+            if(age){
+                user.age = age;
+            }
+            userRepository.save(user)
+            return user;
+        },
+        deleteUser: async(_, {id}, __) => {
+            const userRepository = getRepository(User);
+            const user = await userRepository.findOne({id:id});
+            return await userRepository.remove(user);
+        },
+    },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -6,6 +6,14 @@ type Query {
     user(id: ID): User!
 }
 
+type Mutation {
+    addUser(firstName: String, lastName: String, age: Int): User!
+
+    deleteUser(id: ID!): User!
+
+    updateUser(id: ID!, firstName: String, lastName: String, age: Int): User!
+}
+
 type User {
     id: ID!
     firstName: String
